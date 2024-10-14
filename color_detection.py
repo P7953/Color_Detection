1df8f99f1d87fd07247a77ee689f05f9c3237702
Prathamesh Kadam <85504258+P7953@users.noreply.github.com>
2024-10-14 08:40:54 +0530

    color_detection.py

--- color_detection.py
+++ color_detection.py
@@ -1,61 +1,44 @@
 import cv2
 import numpy as np
 import pandas as pd
-import argparse
 
-ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--image', required=True, help="Image Path")
-args = vars(ap.parse_args())
-img_path = args['image']
+# Load the dataset
+dataset = pd.read_csv('colors.csv')
 
-img = cv2.imread(img_path)
+# Define a function to detect colors
+def detect_colors(image):
+    # Convert the image to RGB format
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-clicked = False
-r = g = b = xpos = ypos = 0
+    # Detect the colors in the image
+    colors = []
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            r, g, b = image[i, j]
+            colors.append((r, g, b))
 
-index=["color","color_name","hex","R","G","B"]
-csv = pd.read_csv('colors.csv', names=index, header=None)
+    # Calculate the RGB values of the detected colors
+    rgb_values = np.array(colors)
 
-def getColorName(R,G,B):
-    minimum = 10000
-    for i in range(len(csv)):
-        d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"]))+ abs(B- int(csv.loc[i,"B"]))
-        if(d<=minimum):
-            minimum = d
-            cname = csv.loc[i,"color_name"]
-    return cname
+    # Determine the color name
+    color_names = []
+    for rgb in rgb_values:
+        min_distance = float('inf')
+        color_name = ''
+        for index, row in dataset.iterrows():
+            distance = np.linalg.norm(rgb - row[['R', 'G', 'B']])
+            if distance < min_distance:
+                min_distance = distance
+                color_name = row['Color']
+        color_names.append(color_name)
 
-def draw_function(event, x,y,flags,param):
-    if event == cv2.EVENT_LBUTTONDBLCLK:
-        global b,g,r,xpos,ypos, clicked
-        clicked = True
-        xpos = x
-        ypos = y
-        b,g,r = img[y,x]
-        b = int(b)
-        g = int(g)
-        r = int(r)
-       
-cv2.namedWindow('image')
-cv2.setMouseCallback('image',draw_function)
+    return color_names
 
-while(1):
+# Load an image
+image = cv2.imread('image.jpg')
 
-    cv2.imshow("image",img)
-    if (clicked):
-   
-        cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
+# Detect the colors in the image
+color_names = detect_colors(image)
 
-        text = getColorName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
-        
-        cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
-
-        if(r+g+b>=600):
-            cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
-            
-        clicked=False
-
-    if cv2.waitKey(20) & 0xFF ==27:
-        break
-    
-cv2.destroyAllWindows()
+# Print the color names
+print(color_names)
